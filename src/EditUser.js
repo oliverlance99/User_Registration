0ddcc7d0916282d217678cec84ac715da5e52631
oliverlance99 <74916346+oliverlance99@users.noreply.github.com>
2021-09-11 12:42:29 +0800

    Front End (Done)

--- src/EditUser.js
+++ src/EditUser.js
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { UserForm } from "./UserForm";
 
 export const EditUser = () => {
-  return <div className="container">Edit User Profile</div>
-}
+	const [user, setUser] = useState();
+
+	useEffect(() => {
+		setUser({
+			lastName: "Kekw" ,
+			firstName: "Pog"
+		})
+	}, [])
+
+	const onSubmit = (data) => {
+		alert(JSON.stringify(data))
+	}
+
+	return user ? (
+		<div className="container">
+			<div className="mt-3">
+				<h3>Edit User Profile</h3>
+				<UserForm user={user} onSubmit={onSubmit}/>
+			</div>
+		</div>
+	) : (
+		<div>Loading...</div>
+	);
+};
